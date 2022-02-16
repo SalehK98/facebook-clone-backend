564608e11a688f8bfa2a601a64c0050d463921e3
“SalehK98” <salehkaloti@gmail.com>
2022-02-16 11:49:24 +0200

    created basic server files

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const router = require("./router");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(router);
+
+app.listen(4000, () => {
+  console.log("Running on http://localhost:4000");
+});
