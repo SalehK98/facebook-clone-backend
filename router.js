ec0bd3596854dc98a2dfdab9f1c2cf565b949eae
“SalehK98” <salehkaloti@gmail.com>
2022-04-05 14:01:47 +0300

    commit april

--- router.js
+++ router.js
@@ -1,8 +1,94 @@
-const express = require("express");
-const router = express.Router();
+import { Router } from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import multer from "multer";
+import { GridFsStorage } from "multer-gridfs-storage";
+import gridfsStream from "gridfs-stream";
+import bodyParser from "body-parser";
+import path from "path";
+import Pusher from "pusher";
+import Grid from "gridfs-stream";
+const router = Router();
 
-const home = require("./controllers/home");
+// db config mongodb not psql
+gridfsStream.mongo = mongoose.mongo;
+
+const mongoUrI =
+  "mongodb+srv://facebookclone:facebookclone@cluster0.ggrh0.mongodb.net/facebookclone?retryWrites=true&w=majority";
+
+const conn = mongoose.createConnection(mongoUrI, {
+  // useCreateIndex: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+let gfs;
+
+conn.once("open", () => {
+  console.log("DB connected");
+  gfs = gridfsStream(conn.db, mongoose.mongo);
+  gfs.collection("images");
+});
+console.log("no error");
+const storage = new GridFsStorage({
+  url: mongoUrI,
+  file: (req, file) => {
+    return new Promise((resolve, reject) => {
+      console.log("no error1");
+      const filename = `image-${Date.now()}${path.extname(file.originalname)}`;
+      console.log("no error2");
+      const fileInfo = {
+        filename: filename,
+        bucketName: "images",
+      };
+      console.log("no error 3");
+      resolve(fileInfo);
+    });
+  },
+});
+
+const upload = multer({ storage });
+
+mongoose.connect(mongoUrI, {
+  // useCreateIndex: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+import home from "./controllers/home.js";
+import login from "./controllers/login.js";
+import postLogin from "./controllers/postLogin.js";
+import upload_image from "./controllers/upload_image.js";
+import post from "./controllers/post.js";
+import postRetrieve from "./controllers/postRetrieve.js";
+import imagesSingleRetrieve from "./controllers/imagesSingleRetrieve.js";
 
 router.get("/", home);
+router.post("/upload/image", upload.single("file"), upload_image);
+router.post("/upload/post", post);
+router.get("/retrieve/posts", postRetrieve);
+router.get("retrieve/images/single", (req, res) => {
+  gfs.find({ filename: req.query.name }, (err, itesms) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      const readstream = gridfsBucket.openDownloadStream(itesms.filename);
+      readstream.pipe(res);
+    }
+  });
+  //   gfs.files.findOne({ filename: req.query.name }, (err, file) => {
+  //   console.log("here");
+  //   if (err) {
+  //       res.status(500).send(err);
+  //     } else {
+  //         if (!file || file.length === 0) {
+  //             res.status(400).json({ err: "file not found" });
+  //         } else {
+  //     }
+  //   }
+  //   });
+});
+router.get("/login", login);
+router.post("/postlogin", postLogin);
 
-module.exports = router;
+export default router;
--- server.js
+++ server.js
@@ -1,12 +1,162 @@
-const express = require("express");
+import express, { json, urlencoded } from "express";
+// import router from "./router.js";
+import cors from "cors";
+import mongoose from "mongoose";
+import multer from "multer";
+import { GridFsStorage } from "multer-gridfs-storage";
+import gridfsStream from "gridfs-stream";
+import bodyParser from "body-parser";
+import path from "path";
+import Pusher from "pusher";
+import Grid from "gridfs-stream";
+import fs from "fs";
+
 const app = express();
-const router = require("./router");
+const PORT = process.env.PORT || 4000;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+//middlwares
+app.use(cors());
+app.use(json());
+app.use(bodyParser.json());
+app.use(urlencoded({ extended: true }));
 
-app.use(router);
+// db config mongodb not psql
+import mongoPosts from "./postsModel.js";
+import mongoComments from "./commentsModel.js";
+Grid.mongo = mongoose.mongo;
 
-app.listen(4000, () => {
-  console.log("Running on http://localhost:4000");
+const mongoUrI =
+  "mongodb+srv://facebookclone:facebookclone@cluster0.ggrh0.mongodb.net/facebookclone?retryWrites=true&w=majority";
+
+const conn = mongoose.createConnection(mongoUrI, {
+  // useCreateIndex: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+let gfs;
+
+conn.once("open", () => {
+  console.log("DB connected");
+  gfs = new mongoose.mongo.GridFSBucket(conn.db, {
+    bucketName: "images",
+  });
+  gfs = Grid(conn.db, mongoose.mongo);
+  gfs.collection("images");
+});
+
+console.log("no error");
+
+const storage = new GridFsStorage({
+  url: mongoUrI,
+  file: (req, file) => {
+    return new Promise((resolve, reject) => {
+      console.log("no error1");
+      const filename = `image-${Date.now()}${path.extname(file.originalname)}`;
+      console.log("no error2");
+      const fileInfo = {
+        filename: filename,
+        bucketName: "images",
+      };
+      console.log("no error 3");
+      resolve(fileInfo);
+    });
+  },
+});
+
+const upload = multer({ storage });
+
+mongoose.connect(mongoUrI, {
+  // useCreateIndex: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+app.post("/upload/image", upload.single("file"), (req, res) => {
+  res.status(201).send(req.file);
+});
+app.post("/upload/post", (req, res) => {
+  const dbPost = req.body;
+
+  mongoPosts.create(dbPost, (err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(201).send(data);
+    }
+  });
+});
+
+app.post("/upload/comments", (req, res) => {
+  const dbComment = req.body;
+
+  mongoComments.create(dbComment, (err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(201).send(data);
+    }
+  });
+});
+
+app.get("/retrieve/posts", (req, res) => {
+  mongoPosts.find((err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      data.sort((b, a) => {
+        return a.timestamp - b.timestamp;
+      });
+      res.status(200).send(data);
+    }
+  });
+});
+
+app.get("/retrieve/comments", (req, res) => {
+  mongoComments.find((err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      data.sort((b, a) => {
+        return a.timestamp - b.timestamp;
+      });
+      res.status(200).send(data);
+    }
+  });
+});
+
+app.get("/retrieve/images/single", (req, res) => {
+  gfs.files.findOne({ filename: req.query.name }, (err, file) => {
+    console.log("here");
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      console.log("now are here");
+      if (!file || file.length === 0) {
+        res.status(400).json({ err: "file not found" });
+      } else {
+        const readstream = gfs.createReadStream(file.filename);
+        readstream.pipe(res);
+      }
+    }
+  });
+});
+
+app.get("/login", (req, res) => {
+  res.send("login");
+});
+
+app.post("/postlogin", (req, res) => {
+  console.log("post recevied");
+  console.log(req.body);
+  res.send("postLogin");
+});
+
+// app.use(router);
+
+app.listen(PORT, () => {
+  console.log(`Running on http://localhost:${PORT}`);
 });
